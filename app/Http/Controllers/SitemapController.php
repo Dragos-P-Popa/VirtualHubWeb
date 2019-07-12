9f4b147db980a56e29209ea091be735dbba618e9
SudarshanMahesh <s.mahesh@icloud.com>
2019-07-12 15:06:24 +0200

    Changed: Disallow Certain Urls in Sitemap.

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -6,19 +6,30 @@ use App\Gates;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\JsonResponse;
 use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Tags\Url;
 
 class SitemapController extends Controller {
 	public function generate() {
-		SitemapGenerator::create( url('') )->writeToFile( "sitemap.xml" );
+		SitemapGenerator::create( url( '' ) )
+		                ->hasCrawled( function ( Url $url ) {
+		                	$s = $url->segment( 1 );
+			                if ( $s === 'api' || $s === 'dashboard' || $s === 'password' || $s === 'storage' ) {
+				                return;
+			                }
+
+			                return $url;
+		                } )
+		                ->writeToFile( "sitemap.xml" );
+
 		return JsonResponse::create( [ "status" => "done", "file" => url( 'sitemap.xml' ) ] );
 	}
 
 	public function all_ap_page() {
 
-		$all_ap = DB::select("SELECT icao FROM vh_airports WHERE iata != '' ORDER BY populairity DESC LIMIT 500;");
+		$all_ap = DB::select( "SELECT icao FROM vh_airports WHERE iata != '' ORDER BY populairity DESC LIMIT 500;" );
 
-		return view("vhweb.allapsitemap",  array(
+		return view( "vhweb.allapsitemap", array(
 			"ap" => $all_ap
-		));
+		) );
 	}
 }
