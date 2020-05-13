d88c876f5d3e4cfb82d330b744792f6cc113a11f
Sudarshan Mahesh <s.mahesh@me.com>
2020-05-13 22:41:34 +0200

    Improved Weather + Added Clouds

--- app/Helper.php
+++ app/Helper.php
@@ -6,6 +6,17 @@ namespace App;
 
 class Helper {
     static function ToObject($array) {
-        return (object) $array;
+        return json_decode( json_encode( $array ) );
+    }
+
+    public static function XmlToJSON( $response ) {
+        $response = str_replace( array( "\n", "\r", "\t" ), '', $response );
+        $response = trim( str_replace( '"', "'", $response ) );
+        $response = simplexml_load_string( $response );
+        $response = json_encode( $response );
+        $response = str_replace( "@attributes", "attributes", $response );
+        $response = json_decode( $response, true );
+
+        return Helper::ToObject( $response["data"] );
     }
 }
--- app/Weather.php
+++ app/Weather.php
@@ -1,155 +1,112 @@
 <?php
 
+
 namespace App;
 
-class Weather {
+use Illuminate\Database\Eloquent\Model;
 
-	private static $weather_condition = [ "Good" => 0, "Moderate" => 0, "Bad" => 0 ];
+class Weather extends Model {
 
-	static function ForAirport( $icao ) {
-		$ctx = stream_context_create( array(
-			'http' =>
-				array(
-					'timeout' => 1.5,  //1200 Seconds is 20 Minutes
-				)
-		) );
+    static function ForAirport( string $icao ) {
+        return self::LoadWeather($icao);
+    }
 
-		$fileContents = @file_get_contents( "https://aviationweather.gov/adds/dataserver_current/httpparam?requestType=retrieve&dataSource=metars&hoursBeforeNow=2&stationString=" . $icao . "&format=xml", false, $ctx );
+    private static function LoadWeather( string $icao ) {
+        $api_url = "https://aviationweather.gov/adds/dataserver_current/httpparam?requestType=retrieve&dataSource=metars&hoursBeforeNow=2&stationString=" . $icao . "&format=xml";
+        $result = [];
+        $dataAvailable = false;
 
-		if ( !$fileContents ) {
-			return array();
-		}
+        $ctx = stream_context_create( array(
+            'http' => array(
+                'timeout' => 1.5,
+            )
+        ) );
 
-		$fileContents = str_replace( array( "\n", "\r", "\t" ), '', $fileContents );
-		$fileContents = trim( str_replace( '"', "'", $fileContents ) );
-		$simpleXml    = simplexml_load_string( $fileContents );
-		$json         = json_encode( $simpleXml );
-		$array        = json_decode( $json, true );
+        $apiResponse = @file_get_contents( $api_url, false, $ctx );
 
-		if ( $array["data"]["@attributes"]["num_results"] == 0 ) {
-			return array();
-		} else {
-			if (isset($array["data"]["METAR"][0])) {
-				return self::parseWeather( $array["data"]["METAR"][0] );
-			} else {
-				return self::parseWeather( $array["data"]["METAR"]);
-			}
-		}
-	}
+        if($apiResponse) {
+            $apiResponse = Helper::XmlToJSON($apiResponse);
 
-	static private function parseWeather( $response )
-    {
-        $weatherMinifiedJson = [];
+            if (isset($apiResponse->attributes->num_results)) {
+                if($apiResponse->attributes->num_results != 0) {
+                    $dataAvailable = true;
+                    $apiResponse = $apiResponse->METAR[0];
+                }
+            }
+        }
 
-            $weatherMinifiedJson["temperature"]["value_c"] = round($response["temp_c"]);
-            $weatherMinifiedJson["temperature"]["value_f"] = round($response["temp_c"] * 9 / 5 + 32);
-            $weatherMinifiedJson["temperature"]["condition"] = "Good";
+        if($dataAvailable) {
+            if (isset($apiResponse->temp_c)) {
+                $celcius = round($apiResponse->temp_c);
+                $farenheid = round($celcius * 9 / 5 + 32);
+                $condition = "Bad";
 
-            $weatherMinifiedJson["visibility"]["value_km"] = round($response["visibility_statute_mi"] * 1.609344);
-            $weatherMinifiedJson["visibility"]["value_m"] = round($response["visibility_statute_mi"], 1);
-            $weatherMinifiedJson["visibility"]["condition"] = "Good";
+                if($celcius > - 40 && $celcius < 52) {
+                    $condition = "Good";
+                }
 
-            $weatherMinifiedJson["pressure"]["value_hg"] = round($response["altim_in_hg"], 1);
-            $weatherMinifiedJson["pressure"]["value_hpa"] = round($response["altim_in_hg"] / 0.02953);
-            $weatherMinifiedJson["pressure"]["condition"] = "Good";
+                $result[] = ["Temperature", $celcius . " °C (" . $farenheid . " °F)", $condition];
+            }
 
-            $weatherMinifiedJson["wind"]["value_kts"] = round($response["wind_speed_kt"]);
-            $weatherMinifiedJson["wind"]["value_kmh"] = round($response["wind_speed_kt"] * 1.85200);
-            $weatherMinifiedJson["wind"]["value_heading"] = round($response["wind_dir_degrees"]);
-            $weatherMinifiedJson["wind"]["condition"] = "Bad";
+            if (isset($apiResponse->visibility_statute_mi)) {
+                $miles = round($apiResponse->visibility_statute_mi, 1);
+                $kilometers = round($miles * 1.609344);
+                $condition = "Bad";
+
+                if($kilometers >= 7) {
+                    $condition = "Good";
+                }
+
+                if($kilometers >= 3 && $kilometers <= 6) {
+                    $condition = "Moderate";
+                }
+
+                $result[] = ["Visibility", $kilometers . " KM (" . $miles . " Mi)", $condition];
+            }
+
+            if (isset($apiResponse->altim_in_hg)) {
+                $hg = round( $apiResponse->altim_in_hg, 1 );
+                $hpa = round( $hg / 0.02953 );
+                $condition = "Good";
+                $result[] = ["Pressure", $hg . " inHg (" . $hpa . " hPa)", $condition];
+            }
+
+            if (isset($apiResponse->wind_speed_kt)) {
+                $kts = round($apiResponse->wind_speed_kt);
+                $kmh = round($kts * 1.85200);
+                $deg = round($apiResponse->wind_dir_degrees);
+                $condition = "Bad";
+
+                if($kts >= 11 && $kts <= 19) {
+                    $condition = "Moderate";
+                }
+
+                if($kts <= 10) {
+                    $condition = "Good";
+                }
+
+                $result[] = ["Wind", $deg . "° at " . $kts . " KTS (" . $kmh . " Km/H)", $condition];
+            }
+
+            if (isset($apiResponse->sky_condition)) {
+                $metar = new Metar($apiResponse->raw_text, false, false);
+                $metar = Helper::ToObject($metar->parse());
+
+                if(isset($metar->clouds_report)) {
+                    $report = $metar->clouds_report . ".";
+
+                    $condition = "Unknown";
+                    $result[] = ["Clouds", $report, $condition];
+                }
+            }
 
 
-            $weatherMinifiedJson["station"] = $response["station_id"];
-            $weatherMinifiedJson["metar"] = $response["raw_text"];
+            if (isset($apiResponse->raw_text)) {
+                $result[] = ["Metar", $apiResponse->raw_text , "Unknown"];
+            }
 
+        }
 
-            $visVal = $weatherMinifiedJson["visibility"]["value_km"];
-            $tempVal = $weatherMinifiedJson["temperature"]["value_c"];
-            $windVal = $weatherMinifiedJson["wind"]["value_kts"];
-
-
-		switch ( $visVal ) {
-			case $visVal >= 7:
-				$weatherMinifiedJson["visibility"]["condition"] = "Good";
-				self::$weather_condition["Good"] ++;
-				break;
-			case $visVal >= 3 && $visVal <= 6:
-				$weatherMinifiedJson["visibility"]["condition"] = "Moderate";
-                self::$weather_condition["Moderate"] ++;
-				break;
-			case $visVal <= 2:
-				$weatherMinifiedJson["visibility"]["condition"] = "Bad";
-                self::$weather_condition["Bad"] ++;
-				break;
-		}
-
-		switch ( $tempVal ) {
-			case $tempVal > - 40 && $tempVal < 52:
-				$weatherMinifiedJson["temperature"]["condition"] = "Good";
-                self::$weather_condition["Good"] ++;
-				break;
-			default:
-				$weatherMinifiedJson["temperature"]["condition"] = "Bad";
-                self::$weather_condition["Bad"] ++;
-				break;
-		}
-
-		switch ( $windVal ) {
-			case $windVal >= 20:
-				$weatherMinifiedJson["wind"]["condition"] = "Bad";
-                self::$weather_condition["Bad"] ++;
-				break;
-			case $windVal >= 11 && $windVal <= 19:
-				$weatherMinifiedJson["wind"]["condition"] = "Moderate";
-                self::$weather_condition["Moderate"] ++;
-				break;
-			case $windVal <= 10:
-				$weatherMinifiedJson["wind"]["condition"] = "Good";
-                self::$weather_condition["Good"] ++;
-				break;
-		}
-
-		$overall_condition = array_search( max( self::$weather_condition ), self::$weather_condition );
-
-		$weatherMinifiedJson["wind"]["value_app"]        = $weatherMinifiedJson["wind"]["value_heading"] . "° at " . $weatherMinifiedJson["wind"]["value_kts"] . " KTS (" . $weatherMinifiedJson["wind"]["value_kmh"] . " Km/H)";
-		$weatherMinifiedJson["pressure"]["value_app"]    = $weatherMinifiedJson["pressure"]["value_hg"] . " inHg (" . $weatherMinifiedJson["pressure"]["value_hpa"] . " hPa)";
-		$weatherMinifiedJson["visibility"]["value_app"]  = $weatherMinifiedJson["visibility"]["value_km"] . " KM (" . $weatherMinifiedJson["visibility"]["value_m"] . " Mi)";
-		$weatherMinifiedJson["temperature"]["value_app"] = $weatherMinifiedJson["temperature"]["value_c"] . " °C (" . $weatherMinifiedJson["temperature"]["value_f"] . " °F)";
-
-		$weatherMinifiedJson = [
-			[
-				"Temperature",
-				$weatherMinifiedJson["temperature"]["value_app"],
-				$weatherMinifiedJson["temperature"]["condition"]
-			],
-			[
-				"Visibility",
-				$weatherMinifiedJson["visibility"]["value_app"],
-				$weatherMinifiedJson["visibility"]["condition"]
-			],
-			[
-				"Pressure",
-				$weatherMinifiedJson["pressure"]["value_app"],
-				$weatherMinifiedJson["pressure"]["condition"]
-			],
-			[
-				"Wind",
-				$weatherMinifiedJson["wind"]["value_app"],
-				$weatherMinifiedJson["wind"]["condition"]
-			],
-			[
-				"Metar",
-				$weatherMinifiedJson["metar"],
-				$overall_condition
-			],
-			[
-				"Station",
-				$weatherMinifiedJson["station"],
-				'Unknown'
-			]
-		];
-
-		return $weatherMinifiedJson;
-	}
-
+        return $result;
+    }
 }
